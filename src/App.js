42a96689cd0262f3de0c373a91a59cc47fa02366
paivabenja <paivabenja05@gmail.com>
2022-05-12 09:01:37 -0300

    bootstrap

--- src/App.js
+++ src/App.js
@@ -1,21 +1,12 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import Topbar from "./Topbar";
+import "bootstrap/dist/css/bootstrap.min.css";
+import "./App.css";
 
-class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
-        </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
-    );
-  }
+export default function App() {
+  return (
+    <div className="App">
+      <Topbar />
+    </div>
+  );
 }
-
-export default App;
